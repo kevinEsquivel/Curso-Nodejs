b24deeadc46cb628bf12bcb3272bdaa858987635
kevinEsquivel <chivatico2@gmail.com>
2022-08-19 15:48:47 -0600

    Agregando el hbs que te permite tener el mismo header en las paginas html, tiene mas usos

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,20 +1,36 @@
 const express = require('express')
-const app = express()
 const path = require('path')//es elmodulo de path
+const hbs = require('hbs');
 
+
+const app = express()
 const port = 8080
 
+//TODO: requise('hbs')  
+//*HANDLEBARS
+app.set('view engine', 'hbs');
+
+//!!!!!!!!!!!!!!!!!!!!!!!IMPORTANTICIMO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+hbs.registerPartials(__dirname + '/views/partials');
 //para poder agregar el html a la ruta
 //!siempre buscara primera mente el archivo index.js si no se encuentra, dara la ruta *
-//?path te permite trabajar con archivos de windos, JOIN es para unir, __DIRNAME->nombre del directorio, y por ultimo el public es la carpeta de los archivos principales
+//?path te permite trabajar con archivos de windos
+//?JOIN es para unir, __DIRNAME->nombre del directorio,
+//?public es la carpeta de los archivos principales
 app.use(express.static(path.join(__dirname, 'public'))); 
 //esto es para cuando es contenido estatico
 
 //la carpeta  public tiene ventaja sobre las rutas
 
 app.get('/', (req, res) => {
-  res.send('Hello  / kevin')
+    res.render('home',{
+        nombre:'Kevin',
+        edad:20
+    });//*para Handlebars.js, la segunda es para las opciones
+  //res.send('Hello  / kevin')
 })
+
+//! se quito el generic.html cuanto tambien en los <a> se cambia por estas rutas
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname+'/public/generic.html'); 
 })
